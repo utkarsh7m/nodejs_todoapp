2b2e99596072b0fb6bcf3d95992bcfb76aedfe0d
Utkasrh Mishra <utkarsh7m@gmail.com>
2023-10-15 20:37:44 +0530

    fixed engine

--- data/database.js
+++ data/database.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
+// import { MongoClient, ServerApiVersion } from 'mongodb'
 
 export const dataBase =() => {
+
     mongoose.connect(process.env.MONGO_URL,{dbName:"backendApi"})
     //connection ke time per then-catch
-    .then(() => {
-      console.log('Connected to MongoDB')
+    .then((c) => {
+      console.log(`Connected to MongoDB with ${c.connection.host}`)
     })
     .catch((e) => {console.log(e);})
     
