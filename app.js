f5ba3970efa4a9dfe80b7fb360cc18c4b1f420cc
Utkarsh Mishra <79663484+utkarsh7m@users.noreply.github.com>
2023-10-25 08:34:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ config({
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: 'https://node-todoapp-react-d19h3nmyy-utkarshs-projects-f932a06a.vercel.app',
+    origin: [process.env.FRONTEND_URL],
     methods: ["GET","POST","PUT","DELETE"],
     credentials: true,
 }))
